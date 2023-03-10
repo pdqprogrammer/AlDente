b6de68923add2686586789b6606ea66ff2ae174f
PaulPrograms <pauldquinones@gmail.com>
2023-03-10 12:05:44 -0600

    Updates for states and physics

--- Assets/Scripts/GameStates.cs
+++ Assets/Scripts/GameStates.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStates : MonoBehaviour
+public enum GameStates
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    START,
+    MENU,
+    INGAME,
+    PAUSE,
+    GAMEOVER,
+    WIN
 }
