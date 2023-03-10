1780e6f163c640dd1d64d7d42dbd9f6d5a93f347
PaulPrograms <pauldquinones@gmail.com>
2023-03-10 13:07:47 -0600

    Updated game manager script for handling different states

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager _Instance;
+
+    private GameStates gameState = GameStates.START;
+
+    /**UNITY FUNCTIONS**/
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    ///PUBLIC FUNCTIONS///
+    public static void ChangeGameState(GameStates state)
     {
-        
+        _Instance.gameState = state;
+    }
+
+    ///SETUP SINGLETON SAFELY///
+    private static GameManager Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                Debug.LogError("GameManager getter - Attempt to get value of GameManager before it has been set.");
+                return null;
+            }
+            return _Instance;
+        }
+        set
+        {
+            if (_Instance != null)
+            {
+                Debug.LogError("GameManager setter - Attempt to set GameManager when it has already been set.");
+            }
+            _Instance = value;
+        }
     }
 }
