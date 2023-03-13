6d660f79f74718faf30f22200df3f6eeba1fc16d
PaulPrograms <pauldquinones@gmail.com>
2023-03-12 20:23:16 -0500

    camera and follower offset

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -6,6 +6,7 @@ public class Follower : MonoBehaviour
 {
     //The thing we want to follow
     public Transform followedTransform;
+    public Vector3 offSet = Vector3.zero;
 
     // Update is called once per frame
     void Update()
@@ -13,7 +14,7 @@ public class Follower : MonoBehaviour
         //Set our position equal to the position of the player
         if (followedTransform != null)
         {
-            Vector3 newPosition = followedTransform.position;
+            Vector3 newPosition = followedTransform.position + offSet;
             newPosition.z = transform.position.z;
             newPosition.x = transform.position.x;
 
