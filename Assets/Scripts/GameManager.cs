d19f4a587c940dcdf742c52deec71dfa902a3dff
PaulPrograms <pauldquinones@gmail.com>
2023-03-14 11:54:51 -0500

    remove defaulting to in game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,7 +26,6 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-        gameState = GameStates.INGAME;//TODO remove after testing stage
         ChangeGameState(CurrentGameState);
     }
 
